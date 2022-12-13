8fabe211b367b712c7a31d69a487688a2ab89594
iiEthan <ethangibbs2@icloud.com>
2022-12-13 01:41:34 +0000

    Added SSL certificate

--- mainApp.js
+++ mainApp.js
@@ -1,15 +1,35 @@
 import express from 'express'
 import vhost from 'vhost'
+import fs from 'fs'
+import https from 'https'
 import { cams } from './camsApp.js'
 
-const domain = 'localhost'
-const port = 3000
+const domain = 'ethangibbs.me'
+const httpPort = 80
+const httpsPort = 443
 const mainApp = express()
 
 // Route cams subdomain
 mainApp.use(vhost(`cams.${domain}`, cams))
-mainApp.use(express.static("public/main")) 
-cams.use(express.static("public/OpenCams")) 
+mainApp.use(express.static('public/main')) 
+cams.use(express.static('public/OpenCams')) 
 
-//Start up express server
-mainApp.listen(port, () => console.log(`listening at ${port}`)) 
+// Read the SSL certificate and key
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/ethangibbs.me/privkey.pem', 'utf8')
+const certificate = fs.readFileSync('/etc/letsencrypt/live/ethangibbs.me/cert.pem', 'utf8')
+
+// Create an HTTPS server
+const server = https.createServer({
+  key: privateKey,
+  cert: certificate,
+}, mainApp)
+
+// Listen for HTTPS requests on the main domain
+server.listen(httpsPort, 'www.ethangibbs.me', () => {
+  console.log('HTTPS server is running on port ' + httpsPort)
+})
+
+// Listen for HTTP requests on the subdomains
+mainApp.listen(httpPort, 'cams.ethangibbs.me', () => {
+  console.log('HTTP server is running on port ' + httpPort)
+})
