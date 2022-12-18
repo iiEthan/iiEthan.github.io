a3e1280ae975ff986f1e8da474673d833a3b9a5d
iiEthan <ethangibbs2@icloud.com>
2022-12-18 16:56:05 -0600

    Redundant code

--- public/main/index.js
+++ public/main/index.js
@@ -1,15 +1,24 @@
-// Fade in title card text
 window.onload = function() {
+    
+    // Fade in items on open
     const fadeCollection = Object.values(document.getElementsByClassName("fade-up"))
     
     fadeCollection.forEach(element => {
-        for (const element of fadeCollection) {
-            element.classList.add('visible')
-        }    
+        element.classList.add('visible')
+    })
+    
+    // Add skews to content
+    const skews = Object.values(document.getElementsByClassName("seperator-skew"))
+
+    skews.forEach(element => {
+        element.innerHTML = 
+        "<svg x='0' y='0' viewBox='0 0 2560 100' preserveAspectRatio='none' version='1.1' " +
+         "xmlns='http://www.w3.org/2000/svg' style='position: absolute;bottom: -1px;'> " +
+         "<polygon points='2560 0 2560 100 0 100'></polygon></svg>"
     })
 }
 
-// Navbar change when scrolling
+// Navbar sticky when scrolling
 window.addEventListener("scroll", function() {
     var header = this.document.querySelector("nav")
     header.classList.toggle("sticky", window.scrollY > 0)
