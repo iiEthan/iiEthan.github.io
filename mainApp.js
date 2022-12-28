d5dd4a44d5cc2c3d058b1909020d3604119d9f9d
iiEthan <ethangibbs2@icloud.com>
2022-12-28 22:56:37 +0000

    Automatic HTTPS redirec

--- mainApp.js
+++ mainApp.js
@@ -2,17 +2,19 @@ import express from 'express'
 import vhost from 'vhost'
 import fs from 'fs'
 import https from 'https'
+import httpToHttps from 'express-http-to-https'
 import { cams } from './camsApp.js'
 
 const domain = 'ethangibbs.me'
 const httpPort = 80
-const httpsPort = 443
+const httpsPort = 443 
 const mainApp = express()
 
 // Route cams subdomain
 mainApp.use(vhost(`cams.${domain}`, cams))
+mainApp.use(vhost(`${domain}`, httpToHttps.redirectToHTTPS()))
 mainApp.use(express.static('public/main')) 
-cams.use(express.static('public/OpenCams')) 
+cams.use(express.static('public/OpenCams'))  
 
 // Read the SSL certificate and key
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/ethangibbs.me/privkey.pem', 'utf8')
@@ -25,11 +27,11 @@ const server = https.createServer({
 }, mainApp)
 
 // Listen for HTTPS requests on the main domain
-server.listen(httpsPort, 'www.ethangibbs.me', () => {
+server.listen(httpsPort, `${domain}`, () => {
   console.log('HTTPS server is running on port ' + httpsPort)
 })
 
-// Listen for HTTP requests on the subdomains
-mainApp.listen(httpPort, 'cams.ethangibbs.me', () => {
+// Listen for HTTP requests on the subdomain
+mainApp.listen(httpPort, `cams.${domain}`, () => {
   console.log('HTTP server is running on port ' + httpPort)
 })
