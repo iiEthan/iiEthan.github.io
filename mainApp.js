48417332690ecf3cf02bb89a942cbb1eab609a93
iiEthan <ethangibbs2@icloud.com>
2022-12-29 11:19:37 -0600

    Put OpenCams in own repo

--- mainApp.js
+++ mainApp.js
@@ -2,28 +2,26 @@ import express from 'express'
 import vhost from 'vhost'
 import fs from 'fs'
 import https from 'https'
-import httpToHttps from 'express-http-to-https'
 import { cams } from './camsApp.js'
 
-const domain = 'ethangibbs.me'
-const httpPort = 80
-const httpsPort = 443 
+const domain = 'localhost' // ethangibbs.me
+const httpPort = 3000 // 80
+const httpsPort = 3001 // 443
 const mainApp = express()
 
 // Route cams subdomain
 mainApp.use(vhost(`cams.${domain}`, cams))
-mainApp.use(vhost(`${domain}`, httpToHttps.redirectToHTTPS()))
 mainApp.use(express.static('public/main')) 
-cams.use(express.static('public/OpenCams'))  
+cams.use(express.static('public/OpenCams')) 
 
 // Read the SSL certificate and key
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/ethangibbs.me/privkey.pem', 'utf8')
-const certificate = fs.readFileSync('/etc/letsencrypt/live/ethangibbs.me/cert.pem', 'utf8')
+//const privateKey = fs.readFileSync('/etc/letsencrypt/live/ethangibbs.me/privkey.pem', 'utf8')
+//const certificate = fs.readFileSync('/etc/letsencrypt/live/ethangibbs.me/cert.pem', 'utf8')
 
 // Create an HTTPS server
 const server = https.createServer({
-  key: privateKey,
-  cert: certificate,
+  //key: privateKey,
+  //cert: certificate,
 }, mainApp)
 
 // Listen for HTTPS requests on the main domain
@@ -31,7 +29,7 @@ server.listen(httpsPort, `${domain}`, () => {
   console.log('HTTPS server is running on port ' + httpsPort)
 })
 
-// Listen for HTTP requests on the subdomain
+// Listen for HTTP requests on the subdomains
 mainApp.listen(httpPort, `cams.${domain}`, () => {
   console.log('HTTP server is running on port ' + httpPort)
 })
